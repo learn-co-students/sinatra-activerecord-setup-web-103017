e4666e07303c2a83c035122f49800334fdf42420
Josh-Stillman <joshstillman@gmail.com>
2017-11-22 14:28:55 -0500

    Done.

---
+++ db/migrate/20171122192622_create_dogs.rb
@@ -0,0 +1,26 @@
+class CreateDogs < ActiveRecord::Migration
+  # def change
+  #
+  # end
+
+  # def up
+  #   create_table :dogs do |t|
+  #     t.string :name
+  #     t.string :breed
+  #   end
+  # end
+  #
+  # def down
+  #   drop_table :dogs
+  # end
+
+  def change
+    create_table :dogs do |t|
+      t.string :name
+      t.string :breed
+    end
+  end
+
+
+
+end
